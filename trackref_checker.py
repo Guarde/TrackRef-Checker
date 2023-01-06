54a2bff89421bc7fb5f3c1ec43cac9c86ac498ad
Guardie <marik.manuel@gmx.at>
2023-01-07 00:18:55 +0100

    A

--- trackref_checker.py
+++ trackref_checker.py
@@ -2,10 +2,10 @@ import os, json, time
 
 def write(text:str):
     try:
-        with open(os.path.join(dir, "trackRefs.txt"), "x") as f:
+        with open(os.path.join(dir, "trackRefs.txt"), mode="x", encoding="utf-8") as f:
             f.write(text)
     except FileExistsError:
-        with open(os.path.join(dir, "trackRefs.txt"), "w") as f:
+        with open(os.path.join(dir, "trackRefs.txt"), mode="w", encoding="utf-8") as f:
             f.write(text)
     except:
         quit()
@@ -22,7 +22,7 @@ for folder in os.listdir(dir):
             songfile = os.path.join(subdir, "song.tmb")
             shortpath = os.path.join(folder, "song.tmb")
             song = None
-            with open(songfile, "r") as f:
+            with open(songfile, mode="r", encoding="utf-8") as f:
                 try:
                     song = json.loads(f.read())
                 except:
